6f4ce8b586c5d9a02996a34f14012deff814faa6
sundaran1122 <sundaran11220@gmail.com>
2021-02-22 02:25:50 +0530

    initial commit

---
+++ meme-scrapper.zsh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+curl -s -H "User-agent: memescraper 1.0" https://www.reddit.com/r/$1/hot.json?limit=$2 > meme.json
+cat meme.json | jq '.' > meme-clean.json
+rm meme.json
+
+cat meme-clean.json | grep url_overridden_by_dest | cut -d\" -f 4 > urls.txt
+rm meme-clean.json
+
+cd $1
+
+for meme in $(cat ../urls.txt)
+do
+  wget $meme &
+done
+
+rm ../urls.txt
